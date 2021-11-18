a76a4c0e9678011be700f887f9868d061b35cf53
jasmin tamang <jasmin.softtechpark@gmail.com>
2021-11-18 09:34:23 +0545

    first commit

---
+++ src/components/addBlog.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="add-blog">
+    <h1>Add Blog</h1>
+    <form>
+        <label for="title">TItle</label>
+        <input type="text" required><br>
+        <label for="textarea">Content</label>
+        <textarea required></textarea>
+    </form>
+    <div id="preview">
+        <h2>Preview Blog</h2>
+        <p>Blog Title:</p>
+        <p>Blog Content:</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+#add-blog *{
+    box-sizing: border-box;
+}
+ #add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+h3{
+    margin-top: 10px;
+} 
+</style>
